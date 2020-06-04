dc053e78aa0dd9e95833491854f095b38793ee7a
Netaly Ramirez <21netaly@gmail.com>
2020-06-04 21:15:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   current_element = 0
   while current_element < array.length do
-    Math.sqrt(array[current_element])
+    array[current_element] * array[current_element]
     current_element +=1
   end
   return array
